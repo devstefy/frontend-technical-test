31783743f511abf2fba33e51d1eb121cb0103a4d
Stefani Pérez <p.stefani16@gmail.com>
2024-09-25 00:12:46 -0400

    feat: build

--- src/constants/testimonials.const.tsx
+++ src/constants/testimonials.const.tsx
@@ -9,7 +9,7 @@ export const testimonials = [
         company: "Quotient"
     },
     {
-        image: "https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        image: "/testimonial-1.png",
         stars: 4,
         testimony: "Lovee the simplicity of the service and the prompt customer support. We can't imagine working without it.",
         name: "Emma Wells",
@@ -17,7 +17,7 @@ export const testimonials = [
         company: "Google"
     },
     {
-        image: "https://images.unsplash.com/photo-1576110598658-096ae24cdb97?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        image: "/testimonial-1.png",
         stars: 5,
         testimony: "Loveee the simplicity of the service and the prompt customer support. We can't imagine working without it.",
         name: "Ian Wells",
